ccd6932cade8c9a6f9e52ae7abbde82e5b54a0e2
Carlos <carlosmendoza99@hotmail.es>
2023-06-26 22:25:28 -0500

    Intro JSX

---
+++ Intro-JSX/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+        <h1>404 NOT FOUND</h1>
+        <div className="contenedor">
+          <img src="scarecrow.png" alt="Espantapajaros"/>
+            <div className="texto">
+              <h2>I have bad news for you</h2>
+              <p>The page you are looking for minght be removed or is temporarily unavailable </p>
+              <button className="boton">BACK TO HOMEPAGE</button>
+            </div>
+        <div className="created"><h3>created by Carlos Mendoza devChallenges.io</h3></div>
+        </div>
+      </div>
+  )
+}
+
+export default App
